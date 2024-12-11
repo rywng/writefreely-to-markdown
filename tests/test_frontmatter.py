fe6706b2ae41fcb168b79f7f35cb5a24499ae4f5
Ryan <ryan@alien.gov>
2024-12-11 20:15:46 +0800

    feat(format): Remove support for description, in favor of <!--more-->
    <!--more--> is supported in many other ssgs like zola, so stripping
    <!--more--> is unnecessary.

--- tests/test_frontmatter.py
+++ tests/test_frontmatter.py
@@ -2,37 +2,8 @@ from writefreely_to_markdown.lib import frontmatter
 from datetime import datetime
 
 
-def test_desc_extraction_no_tags():
-    body = ".. including fonts and cursor themes\n\n\u003c!--more--\u003e\n\n## Cursor themes\n\nRun this:\n\n```bash\nflatpak --user override --filesystem=/usr/share/icons/:ro\n```\n\n## Fonts\n\nInstall `gnome-base/gnome-settings-daemon`\n"
-    assert (
-        frontmatter.get_description_from_body(body)
-        == ".. including fonts and cursor themes."
-    )
-
-    # def test_desc_extraction_has_tag_like():
-    body = "Workaround for [this](https://github.com/go-gitea/gitea/issues/14722#issuecomment-781293156) GitHub problem\n\n\u003c!--more--\u003e\n\n## Create gpg key\n\n``              `bash\nsudo -i -u gitea\ncd data/home\ncat .gitconfig\n```\n\nTo get the default id and email for gitea\n\nGenerate keys for"
-    assert (
-        frontmatter.get_description_from_body(body)
-        == "Workaround for [this](https://github.com/go-gitea/gitea/issues/14722#issuecomment-781293156) GitHub problem."
-    )
-
-    # def test_desc_extraction_has_tag():
-    body = "\u003e Update: On latest version of SwayWM, everything works OOTB with Fcitx5\n\nTo input CJK (Chinese, Japanese and Korean) languages in Linux Minecraft, you can install this mod [CocoaInput](https://legacy.curseforge.com/minecraft/mc-mods/cocoainput).\n\nIf that's the case, I won't bother writing an article about it. The thing is that the mod is only officially available for `1.7` to `1.18`, for the latest Minecraft `1.19` to `1.20`, you'll need to compile the mod binary from an updated fork of it.\n\n#linux #gnome #minecraft\n\n\u003c!--more--\u003e\n\n"
-    tags = ["linux", "gnome", "minecraft"]
-    assert (
-        frontmatter.get_description_from_body(body, tags)
-        == "\u003e Update: On latest version of SwayWM, everything works OOTB with Fcitx5. To input CJK (Chinese, Japanese and Korean) languages in Linux Minecraft, you can install this mod [CocoaInput](https://legacy.curseforge.com/minecraft/mc-mods/cocoainput). If that's the case, I won't bother writing an article about it. The thing is that the mod is only officially available for `1.7` to `1.18`, for the latest Minecraft `1.19` to `1.20`, you'll need to compile the mod binary from an updated fork of it."
-    )
-
-    # def test_desc_extraction_has_tag_no_desc():
-    body = "#linux #gentoolinux #systemd\n\n\u003c!--more--\u003e\n\n"
-    tags = ["gentoolinux", "systemd", "linux"]
-    assert frontmatter.get_description_from_body(body, tags) is None
-
-
 def test_frontmatter_generation():
     expected = """title = "title"
-description = "desc"
 date = 2000-01-03T00:00:00
 updated = 2000-01-03T00:00:00
 draft = true
@@ -43,11 +14,9 @@ authors = ["Test author"]
 tags = ["linux", "pytest"]
 language = "en"
 """
-
     assert (
         frontmatter.get_frontmatter(
             "title",
-            "desc",
             datetime.fromisocalendar(2000, 1, 1),
             datetime.fromisocalendar(2000, 1, 1),
             True,
@@ -58,19 +27,3 @@ language = "en"
         ).as_string()
         == expected
     )
-
-
-def test_frontmatter_content_extraction():
-    # No tags
-    body = ".. including fonts and cursor themes\n\n\u003c!--more--\u003e\n\n## Cursor themes\n\nRun this:\n\n```bash\nflatpak --user override --filesystem=/usr/share/icons/:ro\n```\n\n## Fonts\n\nInstall `gnome-base/gnome-settings-daemon`\n"
-
-    assert (
-        frontmatter.get_content_from_body(body)
-        == "## Cursor themes\n\nRun this:\n\n```bash\nflatpak --user override --filesystem=/usr/share/icons/:ro\n```\n\n## Fonts\n\nInstall `gnome-base/gnome-settings-daemon`"
-    )
-
-    body = "stuff without more"
-    assert frontmatter.get_content_from_body(body) == body
-
-    body = "#linux #gentoolinux #systemd\n\n\u003c!--more--\u003e\n\n### Why I stopped using OpenRC"
-    assert frontmatter.get_content_from_body(body) == "### Why I stopped using OpenRC"
--- writefreely_to_markdown/lib.py
+++ writefreely_to_markdown/lib.py
@@ -13,7 +13,6 @@ class frontmatter:
     @staticmethod
     def get_frontmatter(
         title: str,
-        description: str | None,
         date: datetime,  # Creation date,
         updated: datetime,
         draft: bool,
@@ -26,8 +25,6 @@ class frontmatter:
         doc = document()
         # Ugly but works
         doc.add("title", title)  # type: ignore[arg-type]
-        if description is not None:
-            doc.add("description", description)  # type: ignore[arg-type]
         doc.add("date", date)  # type: ignore[arg-type]
         doc.add("updated", updated)  # type: ignore[arg-type]
         doc.add("draft", draft)  # type: ignore[arg-type]
@@ -39,28 +36,6 @@ class frontmatter:
         taxonomies.add("language", language)
         doc.add("taxonomies", taxonomies)
         return doc
-
-    @staticmethod
-    def get_description_from_body(body: str, tags=None) -> str | None:
-        res = body.split("<!--more-->", 2)
-        if len(res) < 2:
-            return None
-
-        if tags is not None:
-            for tag in tags:
-                res = res[0].split("#" + tag)
-
-        stripped_res = res[0].replace(".\n\n", ". ").replace("\n\n", ". ").strip()
-
-        # In case the result is empty
-        if not stripped_res:
-            return None
-        else:
-            return stripped_res
-
-    @staticmethod
-    def get_content_from_body(body: str) -> str:
-        return body.split("<!--more-->")[-1].strip()
 
 
 def write_json_as_markdown(json_obj, output_dir: str):
@@ -93,7 +68,6 @@ def write_posts(posts, output_dir, author):
             print(out_path)
             post_frontmatter = frontmatter.get_frontmatter(
                 post["title"],
-                frontmatter.get_description_from_body(post["body"], post["tags"]),
                 post["created"],
                 post["updated"],
                 post["draft"],
@@ -104,5 +78,5 @@ def write_posts(posts, output_dir, author):
             )
             write_fp.write("+++\n")
             dump(post_frontmatter, write_fp)
-            write_fp.write("+++\n")
-            write_fp.write(frontmatter.get_content_from_body(post["body"]))
+            write_fp.write("+++\n\n")
+            write_fp.write(post["body"])
